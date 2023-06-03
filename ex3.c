f311b3a792fee06aab3d65b9890bc7f207ab9613
chbornman <101027896+chbornman@users.noreply.github.com>
2023-06-03 07:12:58 -0400

    c_the_hard_way first commit

---
+++ ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+    int i = 97;
+    float x = 3.14;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+    printf("My\tfavorite\nnumbers are \t%d and \t%f.\n",i,x);
+
+    return 0;
+}
